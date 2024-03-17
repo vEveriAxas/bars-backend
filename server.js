c2030cd0ef2d3863583ce079a1f06168c8b27603
AveryOn <lnkonio2@gmail.com>
2024-03-17 21:01:33 +0300

    change-hostname

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const https = require('https');
 const app = express();
 const PORT = 3000;
 const cors = require('cors');
+
+const httpsOptions = {
+    cert: fs.readFileSync(path.resolve(__dirname, './certificate/fullchain.pem')),
+    key: fs.readFileSync(path.resolve(__dirname, './certificate/privkey.pem')),
+};
+
+console.log(httpsOptions);
 
 app.use(cors({
     origin: '*',
@@ -9,9 +19,6 @@ app.use(cors({
     methods: ['POST', 'GET', 'PUT', 'DELETE'],
 }));
 
-app.get('/', (req, res) => {
-    res.send('Екатерина! Вас приветствует будущий сервер Ubuntu v22.0! Добро Пожаловать!');
-});
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://${'localhost'}:${PORT}`);
