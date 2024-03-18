0d77041163e853d35b027b1469a59aa0dbfe4154
AveryOn <lnkonio2@gmail.com>
2024-03-18 11:55:59 +0300

    update-db-connection

---
+++ db_connection/index.js
@@ -0,0 +1,15 @@
+const { Sequelize } = require('sequelize');
+
+// Запуск postgres Сервера
+const sequelize = new Sequelize({
+    dialect: 'postgres',
+    database: 'CHAT',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'Rpc-8456^$=lnk-3104',
+    timezone: '+03:00',
+});
+module.exports = {
+    sequelize
+}
--- server.js
+++ server.js
@@ -5,38 +5,88 @@ const https = require('https');
 const fs = require('fs');
 const http = require('http');
 const cors = require('cors');
-const { Server } = require('socket.io'); 
+const { Server } = require('socket.io');
 const PORT = 443;
 
-// Настройка Cors
-app.use(cors({
-    origin: "https://bars-dusky.vercel.app",
-    // origin: "*",
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-}));
+// =====================================   STATE   ============================================
+const IS_PRODUCTION = false;
 
-// Получение сертификата и ключа SSL
+// DATABASE
+const { sequelize } = require('./db_connection');
+
+// =====================================   DB started   ============================================
+(async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('Successfully connect to postgres database');
+    } catch (err) {
+        throw new Error(`Failed to connect to the database  => ${err}`);
+    }
+})();
+
+// =====================================   Настройка CORS   ============================================
+if (IS_PRODUCTION === true) {
+    app.use(cors({
+        origin: "https://bars-dusky.vercel.app",
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    }));
+} else if (IS_PRODUCTION === false) {
+    app.use(cors({
+        origin: "*",
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    }));
+}
+
+// =====================================   Получение сертификата SSL и приватного ключа   ============================================
+
 const httpsOptions = {
     cert: fs.readFileSync(path.resolve(__dirname, './certificate/fullchain.pem')),
     key: fs.readFileSync(path.resolve(__dirname, './certificate/privkey.pem')),
 };
 
-
-// local
-// const server = http.createServer(app);
-
-// Global  (HTTPS)
-const server = https.createServer(httpsOptions, app).listen(443, () => {
-    console.log(`Server is running on port https:localhost:${PORT}`);
-});
-
-const io = new Server(server, {
-    cors: {
-        origin: "https://bars-dusky.vercel.app",
-        // origin: "http://localhost:8081",
-        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+// =====================================   Запуск сервера   ============================================
+function startedServer(isProduction) {
+    if (isProduction === true) {
+        // Global  (HTTPS)
+        const server = https.createServer(httpsOptions, app).listen(443, () => {
+            console.log(`Server is running on port http:localhost:${PORT}`);
+        });
+        return server;
     }
-})
+    else if (isProduction === false) {
+        // local
+        const server = http.createServer(app);
+        server.listen(3000, () => {
+            console.log(`Server has been started on http//localhost:3000`);
+        });
+        return server;
+    }
+}
+const server = startedServer(IS_PRODUCTION);
+
+// =====================================   Инициализация вебсокет сервера   ============================================
+function initIO(isProduction) {
+    if (isProduction === true) {
+        return new Server(server, {
+            cors: {
+                origin: "https://bars-dusky.vercel.app",
+                methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+            }
+        });
+    }
+    else if (isProduction === false) {
+        return new Server(server, {
+            cors: {
+                origin: "http://localhost:8080",
+                methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+            }
+        });
+    }
+}
+const io = initIO(IS_PRODUCTION);
+
+// =====================================   Обработчики запросов   ============================================
+
 
 app.get('/', (req, res) => {
     res.send('Сервер запущен!');
@@ -50,9 +100,6 @@ io.on('connection', (socket) => {
     });
 });
 
-// Local
-// server.listen(3000, () => {
-//     console.log(`Server has been started on http//localhost:3000`);
-// });
+
 
 
